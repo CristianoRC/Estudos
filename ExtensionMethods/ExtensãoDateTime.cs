3f48dd72f4d6ad5f97493f4ef28d9cbee0c232e2
Cristiano Cunha <Contato@cristianoprogramador.com>
2017-03-19 13:50:07 -0300

    ExtensionMethods

---
+++ ExtensionMethods/ExtensãoDateTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ExtensionMethods
+{
+	internal static class Extension
+	{
+		internal static string ObterDataBrasileira(this DateTime data)
+		{
+			return data.ToString("dd/MM/yyyy HH:mm:ss");
+		}
+
+		internal static int ConverterParaInt(this string Entrada)
+		{
+			return Convert.ToInt32(Entrada);
+		}
+	}
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine(DateTime.Now.ObterDataBrasileira());
+
+			Console.WriteLine("1998".ConverterParaInt());
+		}
+	}
+}
