6a35e9569fe85c6b48a39a4e56ee20181f74aca4
Cristiano Raffi Cunha <contato@cristianoprogramador.com>
2017-05-11 20:33:51 -0300

    01-SQL

---
+++ SQL/Compras/01.sql
@@ -0,0 +1,5 @@
+--Encontre o nome das pessoas que compraram produtos eletrônicos.SELECT c.comprador
+FROM compra c
+INNER JOIN produto p
+    ON c.produto = p.pnome
+WHERE p.categoria = 'eletronicos'
