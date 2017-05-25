1180d4b6ae8e5fb8fd477272636679a0c4f27ace
Cristiano Raffi Cunha <contato@cristianoprogramador.com>
2017-05-25 20:58:25 -0300

    Propiedades Math

---
+++ WEB/JS/Math/main.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    document.getElementById('lnE').innerText = 'Math.E - ' + Math.E;
+    document.getElementById('ln2').innerText = 'Math.LN2 - ' + Math.LN2;
+    document.getElementById('ln10').innerText = 'Math.LN10 - ' + Math.LN10;
+    document.getElementById('log2E').innerText = 'Math.LOG2E - ' + Math.LOG2E
+    document.getElementById('log10E').innerText = 'Math.LOG10E - ' + Math.LOG10E
+    document.getElementById('PI').innerText = 'Math.PI - ' + Math.PI;
+    document.getElementById('SQRT1_2').innerText = 'SQRT1_2 - ' + Math.SQRT1_2;
+    document.getElementById('SQRT2').innerText = 'SQRT2 - ' + Math.SQRT2;
+}
