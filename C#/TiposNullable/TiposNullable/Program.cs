9fde675db072711b47e6acd36baa977a5601ad21
Cristiano Raffi Cunha <contato@cristianoprogramador.com>
2017-05-27 15:46:15 -0300

    Tipos Nullable C#

---
+++ C#/TiposNullable/TiposNullable/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TiposNullable
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int? a = null;
+
+			Console.ForegroundColor = ConsoleColor.DarkBlue;
+			Console.WriteLine("Testes com tipos Nullable.\n" +
+							  "São tipos que normalmente não recebem null, " +
+							  "e com o uso do \' ?\' ele passa a receber null");
+
+			Console.WriteLine("\n----------------\n");
+			try
+			{
+				Console.WriteLine($"O valor de um inteiro com null - {a}");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Ocorreu um erro = {ex.Message}");
+			}
+
+			Console.Write("\n----------------\n");
+
+			Console.WriteLine("\nVerificando se a variavel tem um valor - x.HasValue\n");
+
+			if (!a.HasValue)
+			{
+				Console.WriteLine("O valor da variavel é null.");
+			}
+			else
+			{
+				Console.WriteLine($"O valor da variavel é {a}");
+			}
+
+			Console.WriteLine("\nPassando um valor para a variável - 10\n");
+
+			a = 10;
+
+			if (!a.HasValue)
+			{
+				Console.WriteLine("O valor da variavel é null.");
+			}
+			else
+			{
+				Console.WriteLine($"O valor da variavel é {a}");
+			}
+
+
+
+			Console.ReadKey();
+		}
+	}
+}
