fcea731614e27b3561da09b6451817f96b91b459
Cristiano Raffi Cunha <contato@cristianoprogramador.com>
2017-06-06 10:18:47 -0300

    nameof C#

---
+++ C#/NameOf/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NameOf
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("Método NameOF retorna o nome de uma variavel.\n");
+
+			var nome = "Cristiano Raffi Cunha";
+			var idade = 18;
+
+			Console.WriteLine($"{nameof(nome)}: {nome}");
+			Console.WriteLine($"{nameof(idade)}: {idade}");
+
+			Console.ReadKey();
+		}
+	}
+}
