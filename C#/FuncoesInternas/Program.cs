cf389ec4fa3348b853310b3b62220ff4d84d90d6
Cristiano Raffi Cunha <contato@cristianoprogramador.com>
2017-06-07 20:35:16 -0300

    Funções internas CSharp

---
+++ C#/FuncoesInternas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FuncoesInternas
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+		}
+
+		private bool CompararSenhas(string primeiraSenha, string confirmacaoSenha)
+		{
+			if (!String.IsNullOrEmpty(primeiraSenha) && !String.IsNullOrEmpty(confirmacaoSenha))
+			{
+
+			}
+
+			bool comparar()
+			{
+				if (primeiraSenha == confirmacaoSenha)
+					return true;
+				else
+					return false;
+			}
+
+			return comparar();
+		}
+	}
+}
