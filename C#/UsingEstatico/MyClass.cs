93c3873b7116cc3d41aa40f0904194ad1a0bb8d7
Cristiano Raffi Cunha <contato@cristianoprogramador.com>
2017-06-09 20:42:49 -0300

    UsingEstatico

---
+++ C#/UsingEstatico/MyClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Convert;
+
+namespace UsingEstatico
+{
+	public class MyClass
+	{
+		//Sem a utilização de Using Estático
+		public static int ConverterParaInteiro(string valor)
+		{
+			try
+			{
+				return System.Convert.ToInt32(valor);
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+
+		public static double ConverterParaReais(string valor)
+		{
+			try
+			{
+				return ToDouble(valor);
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+
+		}
+	}
+}
