7522fb756a184bb45a5b3f138fea4cb9ea4385c7
Cristiano Raffi Cunha <contato@cristianoprogramador.com>
2017-06-10 22:38:17 -0300

    RunShellScrpit

---
+++ C#/RunShellScrpit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace RunShellScrpit
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			try
+			{
+				ProcessStartInfo ps = new ProcessStartInfo();
+				ps.FileName = @"Aula.sh";
+				ps.UseShellExecute = false;
+				ps.RedirectStandardOutput = true;
+
+				Process process = Process.Start(ps);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
