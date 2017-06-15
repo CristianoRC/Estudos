28f07b287a6040aa55ea7c1325635f613b93959e
Cristiano Cunha <Contato@cristianoprogramador.com>
2017-06-14 23:50:33 -0300

    Consertando o debugger do monodevelop

---
+++ C#/Exercicios/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Olá Mundo");
+
+            Console.ReadKey();
+        }
+    }
+}
