42a6f6440d8c204c5e0b5cc9f6e198202a8de063
Cristiano Cunha <Contato@cristianoprogramador.com>
2017-06-18 14:48:50 -0300

    LambdaExpression -  LINQ / DotCEP exemplo

---
+++ C#/ExpressaoLambda/ExemploDotCEP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExemploDotCEP
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            IEnumerable<DotCEP.Endereco> enderecos =
+                                  DotCEP.Consultas.ObterListaDeEnderecos(DotCEP.UF.RS, "Pelotas", "Viana");
+
+            var minhaRua = enderecos.
+                                    Where(it => it.logradouro.Contains("Ferreira")).
+                                    First();
+
+            Console.WriteLine($"Rua lozalizada: {minhaRua.logradouro}");
+        }
+    }
+}
