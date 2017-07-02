addb6ba1ed10fdf70c2a74deebab3f179cc382e5
Cristiano Cunha <Contato@cristianoprogramador.com>
2017-07-01 21:46:39 -0300

    Asp Net Core

---
+++ CRUD/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CRUD
+{
+    public class Cliente : IDisposable
+    {
+        public Cliente()
+        {
+        }
+
+        public Cliente(int codigo)
+        {
+            //Construtor
+        }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public char? Sexo { get; set; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CRUD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUD
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
