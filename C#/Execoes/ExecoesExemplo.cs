488f78b619aab961b7348849ce84c7e3cec5b0c6
Cristiano Cunha <Contato@cristianoprogramador.com>
2017-07-07 17:32:13 -0300

    Exception C#

---
+++ C#/Execoes/ExecoesExemplo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Execoes
+{
+    [Serializable]
+    public class ExecoesExemplo : Exception
+    {
+        public ExecoesExemplo()
+        {
+
+        }
+
+        public ExecoesExemplo(string message) : base(message)
+        {
+        }
+
+        public ExecoesExemplo(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ExecoesExemplo(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ C#/Execoes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Execoes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Olá Mundo");
+                throw new ExecoesExemplo("Exemplo de erro C#");
+            }
+            catch (ExecoesExemplo ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
