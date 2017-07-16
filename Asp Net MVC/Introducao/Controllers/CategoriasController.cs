76ff92fc866b0d021d14f8c3b03524e4f6f4258d
Cristiano Cunha <Contato@cristianoprogramador.com>
2017-07-15 23:27:12 -0300

    Create - MVC

--- Asp Net MVC/Introducao/Controllers/CategoriasController.cs
+++ Asp Net MVC/Introducao/Controllers/CategoriasController.cs
@@ -9,20 +9,38 @@ namespace Introducao.Controllers
 {
     public class CategoriasController : Controller
     {
-		#region Lista de Categorias
-		private static IList<Categoria> categorias = new List<Categoria>()
-			{
-				new Categoria() { IdCategoria = 1, Nome = "Notebooks" },
-				new Categoria() { IdCategoria = 2, Nome = "Monitores" },
-				new Categoria() { IdCategoria = 3, Nome = "Impressoras" },
-				new Categoria() { IdCategoria = 4, Nome = "Mouses" },
-				new Categoria() { IdCategoria = 5, Nome = "Desktops" }
-			};
+        #region Lista de Categorias
+        private static IList<Categoria> categorias = new List<Categoria>()
+            {
+                new Categoria() { IdCategoria = 1, Nome = "Notebooks" },
+                new Categoria() { IdCategoria = 2, Nome = "Monitores" },
+                new Categoria() { IdCategoria = 3, Nome = "Impressoras" },
+                new Categoria() { IdCategoria = 4, Nome = "Mouses" },
+                new Categoria() { IdCategoria = 5, Nome = "Desktops" }
+            };
         #endregion
 
         public ActionResult Index()
         {
             return View(categorias);
+        }
+
+        public ActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Criar(Categoria categoria)
+        {
+            categoria.IdCategoria = categorias.Count + 1;
+            categorias.Add(categoria);
+
+            //Inserindo o ID com LINQ
+            //categoria.IdCategoria = categorias.Select(m => m.IdCategoria).Max() + 1;
+
+            return RedirectToAction("index");
         }
     }
 }
