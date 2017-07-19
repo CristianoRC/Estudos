9f53513cfd2725a252b0b5225eedb4597b9152ad
Cristiano Cunha <Contato@cristianoprogramador.com>
2017-07-19 20:29:01 -0300

    CEP MVC

---
+++ Asp Net MVC/Introducao/Controllers/CEPController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DotCEP;
+using Introducao.Models;
+
+namespace Introducao.Controllers
+{
+    public class CEPController : Controller
+    {
+        public ActionResult Index()
+        {
+            var enderecoEx = Consultas.ObterEnderecoCompleto("96083-500");
+
+            return View(enderecoEx);
+        }
+    }
+}
