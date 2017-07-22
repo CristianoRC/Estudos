6bc4ab970620b2b8dfd698f696797e0641fe915c
Cristiano Cunha <Contato@cristianoprogramador.com>
2017-07-22 17:21:20 -0300

    CRUD MVC

--- Asp Net MVC/Introducao/Controllers/CategoriasController.cs
+++ Asp Net MVC/Introducao/Controllers/CategoriasController.cs
@@ -22,7 +22,7 @@ namespace Introducao.Controllers
 
         public ActionResult Index()
         {
-            return View(categorias);
+            return View(categorias.OrderBy(x => x.IdCategoria));
         }
 
         public ActionResult Criar()
@@ -37,10 +37,33 @@ namespace Introducao.Controllers
             categoria.IdCategoria = categorias.Count + 1;
             categorias.Add(categoria);
 
-            //Inserindo o ID com LINQ
-            //categoria.IdCategoria = categorias.Select(m => m.IdCategoria).Max() + 1;
-
             return RedirectToAction("index");
+        }
+
+        public ActionResult Editar(long ID)
+        {
+            var categoriaTemp = categorias.Where(m => m.IdCategoria.Equals(ID)).First();
+
+            return View(categoriaTemp);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Editar(Categoria _categoria)
+        {
+            categorias.Remove(categorias.Where(x => x.IdCategoria == _categoria.IdCategoria).First());
+
+            categorias.Add(_categoria);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Deletar(long ID)
+        {
+            categorias.Remove(categorias.Where(x => x.IdCategoria == ID).First());
+
+            return RedirectToAction("Index");
         }
     }
 }
