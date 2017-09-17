6c6ae7457ef863c6a0c2740c0215b078a6faf203
Cristiano Cunha <Contato@cristianoprogramador.com>
2017-09-17 15:42:22 -0300

    Delgates

---
+++ C#/Delegates/Delegates/Delegates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class Delegates
+    {
+        public void ExecutarExemplo()
+        {
+            Calcular delegateSoma = new Calcular(somar);
+
+            Console.WriteLine($"5 + 6 = {delegateSoma(5, 6)}");
+        }
+
+        delegate int Calcular(int x, int y);
+
+        int somar(int x, int y)
+        {
+            return x + y;
+        }
+
+        int subtrair(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ C#/Delegates/Delegates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Delegates delgs = new Delegates();
+
+            delgs.ExecutarExemplo();
+
+            Console.ReadKey();
+        }
+    }
+}
