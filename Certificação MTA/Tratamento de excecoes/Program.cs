5a3d820f1f067fea71c83c754379f1a15a607a38
Cristiano Cunha <Contato@cristianoprogramador.com>
2018-01-03 15:06:05 -0200

    Certificação - estrutura de repetição e Excecoes

---
+++ Certificação MTA/Tratamento de excecoes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tratamento_de_excecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var entrada = Convert.ToInt32(Console.ReadLine());
+                var resultado = 15/entrada;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine($"Erro de divisão por zero: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro genérico: {e.Message}");
+                //Catch "generico".
+            }
+            finally
+            {
+                Console.WriteLine("Final!");
+            }
+        }
+    }
+}
