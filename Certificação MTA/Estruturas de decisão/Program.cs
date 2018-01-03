5e03ad80b7d3e16faa9b13a8429e48a1f76a1d51
Cristiano Cunha <Contato@cristianoprogramador.com>
2018-01-03 09:22:47 -0200

    Ceertificação - Estrutura de desição : ?

---
+++ Certificação MTA/Estruturas de decisão/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Estruturas_de_decisão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Operadores ?:
+                Condição ? PrimeiraExpressão : SeguindaExpressão;
+                                TRUE                FALSE 
+            */
+            var idade = 17;
+
+            var Maioridade = idade >= 18 ? true : false;
+
+            Console.WriteLine(Maioridade);
+        }
+    }
+}
