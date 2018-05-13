4f5e44fd5012cbc3e20283acd83467c4080c6060
Cristiano Cunha <Contato@cristianoprogramador.com>
2018-05-13 09:25:02 -0300

    Atualizações projeto de educação no trânsito

---
+++ C#/AdivinharNumero/Numeros.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void main()
+{
+    int valorMaximo, valorEscolhido, valorInserido, valorErro;
+    int pontuacao = 100;
+    bool valorEcontrado = false;
+
+    //system("cls");
+    system("clear");
+    printf("Olá, bem vindo, você e capaz de adivinhar o numero que estou pensando?\n");
+    printf("Eu posso escolher um numero Natual de 0 a ");
+    scanf("%d", &valorMaximo);
+
+    // Cada erro vale 100/nº de termos
+    valorErro = pontuacao / valorMaximo;
+
+    // O resto da divisão sempre será menor que o valor que esta sendo dividido;
+    //Se valor maximo for igual a 10 ele geraria um numero de 0 a 9, por isso do +1;
+    valorEscolhido = rand() % (valorMaximo + 1);
+
+    printf("\n\nEscolhendo um numero ...\n\n");
+
+    do
+    {
+        printf("\nInsira um valor: ");
+        scanf("%d", &valorInserido);
+
+        if (valorEscolhido == valorInserido)
+        {
+            valorEcontrado = true;
+        }
+        else if (valorInserido < valorEscolhido)
+        {
+            printf("O valor inserido e menor que o numero que eu escolhi...\n");
+            pontuacao -= valorErro;
+        }
+        else if (valorInserido > valorEscolhido)
+        {
+            printf("O valor inserido e maior que o numero que eu escolhi...\n");
+            pontuacao -= valorErro;
+        }
+
+    } while (!valorEcontrado);
+    
+    printf("Parabéns voce acertou, o valor era %d e voce ficou com %d pontos de 100.\n\n", valorEscolhido, pontuacao);
+}
---
+++ C#/AdivinharNumero/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AdivinharNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorMenor = 0;
+            int valorMaior = 100;
+            int valorAtual;
+            var valorEncontrado = false;
+
+            Console.WriteLine("Olá, bem vindo ao adivinhador de numeros");
+            Console.WriteLine("Pense em um numero de 0 a 100 que o software ira \"adivinhar\" com perguntas simples.");
+
+            do
+            {
+                valorAtual = (valorMaior - valorMenor) / 2;
+
+                Console.Write("\n\n O seu valor é <, > ou = a " + valorAtual + "? ");
+                var resposta = Console.ReadLine().Trim();
+
+                switch (resposta)
+                {
+                    case ">":
+                        valorMenor = valorAtual + 1;
+                        break;
+                    case "<":
+                        valorMaior = valorAtual - 1;
+                        break;
+                    case "=":
+                        valorEncontrado = true;
+                        break;
+                    default:
+                        Console.WriteLine("\"" + resposta + "\"" + " é um valor inválido");
+                        break;
+                }
+
+            } while (!valorEncontrado);
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("\n\nSeu valor foi encontrado -  " + valorAtual);
+
+            Console.ReadKey();
+        }
+    }
+}
