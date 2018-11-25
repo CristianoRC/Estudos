4552659f03d72a2cba8c5029c860db6d5c80bdaa
Cristiano Cunha <Contato@cristianoprogramador.com>
2018-11-25 17:51:01 -0200

    Validação de enum

---
+++ C#/Validando ENUM/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace testes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enum.TryParse((typeof(Cores)), "0", out var saidaConversao);
+
+            Console.WriteLine(saidaConversao == null ? "Valor NULL" : saidaConversao);
+        }
+    }
+
+    public enum Cores
+    {
+        Azul,
+        Preto,
+        Branco
+    }
+}
